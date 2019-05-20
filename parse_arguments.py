ec0eda44bd7605a6cb148a9b54af9bcc6b978ec8
giogia <Giovannitommasi95@gmail.com>
2019-05-20 11:41:41 +0800

    parse arguments

---
+++ parse_arguments.py
@@ -0,0 +1,38 @@
+import json
+import os
+from collections import namedtuple
+
+PATH = 'parameters'
+
+
+def parse_arguments():
+
+    """
+
+    USAGE
+    parameters = parse_arguments()
+    value  = parameters.filename.field
+
+    EXAMPLE
+    parameters = parse_arguments()
+    scale_num = parameters.hyperparams.scale_num
+
+    """
+
+    parameters = {}
+    for file in os.listdir(PATH):
+
+        if file.endswith('.json'):
+            with open(os.path.join(PATH, file)) as json_file:
+
+                file_name = os.path.splitext(file)[0]
+                values = json.load(json_file)
+                values = namedtuple(file_name, values.keys())(**values)
+
+                parameters[file_name] = values
+
+    return namedtuple('parameters', parameters.keys())(**parameters)
+
+
+parameters = parse_arguments()
+print(parameters.hyperparams)
