d46c6219ba21a1d40cedde6baab3f171c66a69cf
giogia <Giovannitommasi95@gmail.com>
2019-05-26 14:49:56 +0800

    Update parse_arguments.py

--- implementation/parse_arguments.py
+++ implementation/parse_arguments.py
@@ -1,4 +1,4 @@
-import configparser
+from localconfig import config
 from collections import namedtuple
 
 PATH = 'parameters.ini'
@@ -17,13 +17,11 @@ def parse_arguments():
 
     """
 
-    config = configparser.ConfigParser()
-
     config.read(PATH)
 
     parameters = {}
 
-    for section in config.sections():
+    for section in config:
         fields = dict(config.items(section))
         fields = namedtuple(section, fields.keys())(**fields)
 
