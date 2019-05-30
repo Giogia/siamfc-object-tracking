a3de4dfe1b5613254d40f7b0b15eb7971f397209
giogia <Giovannitommasi95@gmail.com>
2019-05-30 15:11:56 +0800

    Update inspect_model.py

--- implementation/inspect_model.py
+++ implementation/inspect_model.py
@@ -3,7 +3,3 @@ import tensorflow as tf
 
 # Use this line to print all the tensor in the saved model
 print_tensors_in_checkpoint_file(file_name='model/model', tensor_name='', all_tensors=False, all_tensor_names=True)
-
-
-bn_beta = tf.train.load_variable(ckpt_dir_or_file='model/model', name='batch_normalization/beta')
-print(bn_beta)
