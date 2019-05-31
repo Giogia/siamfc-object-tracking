4773d0c1c30f49754e1a8ead99300c7ff6eca8bb
Alessandro Tonali <alessandro.tonali@mail.polimi.it>
2019-05-31 18:16:37 +0800

    small corrections

--- implementation/tracker.py
+++ implementation/tracker.py
@@ -6,156 +6,144 @@ import time
 
 import siamese_network
 from visualization import show_frame
+from parse_arguments import parameters
 
 
+def tracker(frame_name_list, b_box_x, b_box_y, b_box_width, b_box_height, final_score_size, filename, image,
+            network_z, scores):
 
-
-def tracker(hyperparams, run, design, frame_name_list, pos_x, pos_y, target_w, target_h, final_score_sz, filename, image,
-            templates_z, scores, start_frame):
-
-    print("face")
-    #TODO capiamo che forma ha frame name list, penso sia una riga
     num_frames = len(frame_name_list)
-    # stores tracker's output for evaluation
     b_boxes = np.zeros((num_frames, 4))
 
-    scale_factors = hyperparams.scale_step ** np.linspace(-np.ceil(hyperparams.scale_num / 2), np.ceil(hyperparams.scale_num / 2), hyperparams.scale_num)
-    # cosine window to penalize large displacements    
-    hann_1d = np.expand_dims(np.hanning(final_score_sz), axis=0)
-    penalty = np.transpose(hann_1d) * hann_1d
+    scale_factors = parameters.hyperparameters.scale_step ** \
+                    np.linspace(-np.ceil(parameters.hyperparameters.scale_num / 2), np.ceil(parameters.hyperparameters.scale_num / 2),
+                    parameters.hyperparameters.scale_num)
+
+    # Window to penalize large displacements
+    hann_window = np.expand_dims(np.hanning(final_score_size), axis=0)
+
+    penalty = np.matmul(hann_window, hann_window)
     penalty = penalty / np.sum(penalty)
 
-    context = design.context * (target_w + target_h)
-    z_sz = np.sqrt(np.prod((target_w + context) * (target_h + context)))
+    context = parameters.design.context * (b_box_width + b_box_height)
+    window_size_z = np.sqrt((b_box_width + context) * (b_box_height + context))
+    window_size_x = float(parameters.design.search_sz) / parameters.design.exemplar_sz * window_size_z
 
-    x_sz = float(design.search_sz) / design.exemplar_sz * z_sz
+    init = tf.global_variables_initializer()
 
-    """
-    # thresholds to saturate patches shrinking/growing
-    min_z = hp.scale_min * z_sz
-    max_z = hp.scale_max * z_sz
-    min_x = hp.scale_min * x_sz
-    max_x = hp.scale_max * x_sz
-
-    # run_metadata = tf.RunMetadata()
-    # run_opts = {
-    #     'options': tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE),
-    #     'run_metadata': run_metadata,
-    # }
-    """
-    run_opts = {}
-
-    saver = tf.train.Saver()
-
-    # with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
     with tf.Session() as sess:
-        tf.global_variables_initializer().run()
+        sess.run(init)
+
         # Coordinate the loading of image files.
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(coord=coord)
+        coordinator = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coordinator)
 
-        # save first frame position (from ground-truth)
-        b_boxes[0, :] = pos_x - target_w / 2, pos_y - target_h / 2, target_w, target_h
+        # Save first frame position (from ground-truth)
+        b_boxes[0, :] = b_box_x - b_box_width / 2, b_box_y - b_box_height / 2, b_box_width, b_box_height
 
-        image_, templates_z_ = sess.run([image, templates_z], feed_dict={
-            siamese_network.pos_x_ph: pos_x,
-            siamese_network.pos_y_ph: pos_y,
-            siamese_network.z_sz_ph: z_sz,
+        image, network_z = sess.run([image, network_z], feed_dict={
+            siamese_network.bbox_x_ph: b_box_x,
+            siamese_network.bbox_y_ph: b_box_y,
+            siamese_network.window_size_z_ph: window_size_z,
             filename: frame_name_list[0]})
-        # new_templates_z_ = templates_z_
 
-        #print("saving")
-        saver.save(sess, 'model')
-        #print("saved")
-
-        t_start = time.time()
+        time_start = time.time()
 
         # Get an image from the queue
         for i in range(1, num_frames):
-            scaled_exemplar = z_sz * scale_factors
-            scaled_search_area = x_sz * scale_factors
-            scaled_target_w = target_w * scale_factors
-            scaled_target_h = target_h * scale_factors
-            image_, scores_ = sess.run(
+            scaled_window_size_z = window_size_z * scale_factors
+            scaled_window_size_x = window_size_x * scale_factors
+            scaled_b_box_width = b_box_width * scale_factors
+            scaled_b_box_height = b_box_height * scale_factors
+            image, scores = sess.run(
                 [image, scores],
                 feed_dict={
-                    siamese_network.pos_x_ph: pos_x,
-                    siamese_network.pos_y_ph: pos_y,
-                    siamese_network.x_sz0_ph: scaled_search_area[0],
-                    siamese_network.x_sz1_ph: scaled_search_area[1],
-                    siamese_network.x_sz2_ph: scaled_search_area[2],
-                    templates_z: np.squeeze(templates_z_),
+                    siamese_network.bbox_x_ph: b_box_x,
+                    siamese_network.bbox_y_ph: b_box_y,
+                    siamese_network.window_size_x_0_ph: scaled_window_size_x[0],
+                    siamese_network.window_size_x_1_ph: scaled_window_size_x[1],
+                    siamese_network.window_size_x_2_ph: scaled_window_size_x[2],
+                    network_z: np.squeeze(network_z),
                     filename: frame_name_list[i],
-                }, **run_opts)
-
-            print(tf.GraphKeys.GLOBAL_VARIABLES)
-            print([elem for elem in tf.GraphKeys.TRAINABLE_VARIABLES])
-            print([elem for elem in tf.GraphKeys.LOCAL_VARIABLES])
-
-            scores_ = np.squeeze(scores_)
-            # penalize change of scale
-            scores_[0, :, :] = hyperparams.scale_penalty * scores_[0, :, :]
-            scores_[2, :, :] = hyperparams.scale_penalty * scores_[2, :, :]
-            # find scale with highest peak (after penalty)
-            new_scale_id = np.argmax(np.amax(scores_, axis=(1, 2)))
-            # update scaled sizes
-            x_sz = (1 - hyperparams.scale_lr) * x_sz + hyperparams.scale_lr * scaled_search_area[new_scale_id]
-            target_w = (1 - hyperparams.scale_lr) * target_w + hyperparams.scale_lr * scaled_target_w[new_scale_id]
-            target_h = (1 - hyperparams.scale_lr) * target_h + hyperparams.scale_lr * scaled_target_h[new_scale_id]
-            # select response with new_scale_id
-            score_ = scores_[new_scale_id, :, :]
-            score_ = score_ - np.min(score_)
-            score_ = score_ / np.sum(score_)
-            # apply displacement penalty
-            score_ = (1 - hyperparams.window_influence) * score_ + hyperparams.window_influence * penalty
-            pos_x, pos_y = _update_target_position(pos_x, pos_y, score_, final_score_sz, design.tot_stride,
-                                                   design.search_sz, hyperparams.response_up, x_sz)
-            # convert <cx,cy,w,h> to <x,y,w,h> and save output
-            b_boxes[i, :] = pos_x - target_w / 2, pos_y - target_h / 2, target_w, target_h
-            # update the target representation with a rolling average
-            if hyperparams.z_lr > 0:
-                new_templates_z_ = sess.run([templates_z], feed_dict={
-                    siamese_network.pos_x_ph: pos_x,
-                    siamese_network.pos_y_ph: pos_y,
-                    siamese_network.z_sz_ph: z_sz,
-                    image: image_
                 })
 
-                templates_z_ = (1 - hyperparams.z_lr) * np.asarray(templates_z_) + hyperparams.z_lr * np.asarray(new_templates_z_)
+            scores = np.squeeze(scores)
+            # Penalize change of scale
+            scores[0, :, :] = parameters.hyperparameters.scale_penalty * scores[0, :, :]
+            scores[2, :, :] = parameters.hyperparameters.scale_penalty * scores[2, :, :]
 
-            # update template patch size
-            z_sz = (1 - hyperparams.scale_lr) * z_sz + hyperparams.scale_lr * scaled_exemplar[new_scale_id]
+            # Find scale with highest score
+            best_scale = np.argmax(np.amax(scores, axis=(1, 2)))
 
-            if run.visualization:
-                show_frame(image_, b_boxes[i, :], 1)
+            # Update scaled sizes
+            window_size_x = (1 - parameters.hyperparameters.scale_lr) * window_size_x + parameters.hyperparameters.scale_lr * scaled_window_size_x[best_scale]
+            b_box_width = (1 - parameters.hyperparameters.scale_lr) * b_box_width + parameters.hyperparameters.scale_lr * scaled_b_box_width[
+                best_scale]
+            b_box_height = (1 - parameters.hyperparameters.scale_lr) * b_box_height + parameters.hyperparameters.scale_lr * scaled_b_box_height[
+                best_scale]
 
-        t_elapsed = time.time() - t_start
-        speed = num_frames / t_elapsed
+            # Select response with best scale
+            best_score = scores[best_scale, :, :]
+            best_score = best_score - np.min(best_score)
+            best_score = best_score / np.sum(best_score)
+
+            # Apply displacement penalty
+            best_score = (1 - parameters.hyperparameters.window_influence) * best_score + parameters.hyperparameters.window_influence * penalty
+            b_box_x, b_box_y = update_b_box_position(b_box_x, b_box_y, best_score, final_score_size, window_size_x)
+
+            # Convert <cx,cy,w,h> to <x,y,w,h> and save output
+            b_boxes[i, :] = b_box_x - b_box_width / 2, b_box_y - b_box_height / 2, b_box_width, b_box_height
+
+            # Update the target representation with a rolling average
+            if parameters.hyperparameters.z_lr > 0:
+                new_network_z = sess.run([network_z], feed_dict={
+                    siamese_network.bbox_x_ph: b_box_x,
+                    siamese_network.bbox_y_ph: b_box_y,
+                    siamese_network.window_size_z_ph: window_size_z,
+                    image: image
+                })
+
+                network_z = (1 - parameters.hyperparameters.z_lr) * np.asarray(
+                    network_z) + parameters.hyperparameters.z_lr * np.asarray(
+                    new_network_z)
+
+            # Update template patch size
+            window_size_z = (1 - parameters.hyperparameters.scale_lr) * window_size_z + parameters.hyperparameters.scale_lr * scaled_window_size_z[best_scale]
+
+            if parameters.run.visualization:
+                show_frame(image, b_boxes[i, :], 1)
+
+        time_elapsed = time.time() - time_start
+        speed = num_frames / time_elapsed
 
         # Finish off the filename queue coordinator.
-        coord.request_stop()
-        coord.join(threads)
-
-        # from tensorflow.python.client import timeline
-        # trace = timeline.Timeline(step_stats=run_metadata.step_stats)
-        # trace_file = open('timeline-search.ctf.json', 'w')
-        # trace_file.write(trace.generate_chrome_trace_format())
+        coordinator.request_stop()
+        coordinator.join(threads)
 
     plt.close('all')
 
     return b_boxes, speed
 
 
-def _update_target_position(pos_x, pos_y, score, final_score_sz, tot_stride, search_sz, response_up, x_sz):
-    # find location of score maximizer
-    p = np.asarray(np.unravel_index(np.argmax(score), np.shape(score)))
-    # displacement from the center in search area final representation ...
-    center = float(final_score_sz - 1) / 2
-    disp_in_area = p - center
-    # displacement from the center in instance crop
-    disp_in_xcrop = disp_in_area * float(tot_stride) / response_up
-    # displacement from the center in instance crop (in frame coordinates)
-    disp_in_frame = disp_in_xcrop * x_sz / search_sz
-    # *position* within frame in frame coordinates
-    pos_y, pos_x = pos_y + disp_in_frame[0], pos_x + disp_in_frame[1]
-    return pos_x, pos_y
+def update_b_box_position(b_box_x, b_box_y, score, final_score_size, window_size_x):
+    search_sz = parameters.design.search_sz
+    tot_stride = parameters.design.tot_stride
+    response_up = parameters.hyperparameters.response_up
+
+    # Find location of score maximizer
+    point = np.array(np.unravel_index(np.argmax(score), np.shape(score)))
+
+    # Displacement from the center in search area final representation
+    center = float(final_score_size - 1) / 2
+    disp_in_area = point - center
+
+    # Displacement from the center in instance crop
+    disp_in_window_x = disp_in_area * float(tot_stride) / response_up
+
+    # Displacement from the center in instance crop (in frame coordinates)
+    disp_in_frame = disp_in_window_x * window_size_x / search_sz
+
+    # Position within frame in frame coordinates
+    b_box_y, b_box_x = b_box_y + disp_in_frame[0], b_box_x + disp_in_frame[1]
+
+    return b_box_x, b_box_y
