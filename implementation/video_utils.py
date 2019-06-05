9de987db510b61a0767d02a896f16f89a3176b11
giogia <Giovannitommasi95@gmail.com>
2019-06-05 15:28:51 +0800

    Update video_utils.py

--- implementation/video_utils.py
+++ implementation/video_utils.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import cv2
 from PIL import Image
 
 
@@ -13,10 +14,12 @@ def get_videos(dataset_folder):
 
 def get_frames(video_folder):
 
-    frames_list = [frame for frame in os.listdir(video_folder) if frame.endswith('.jpg')]
-    # frames_list = [os.path.join(parameters.environment.dataset_folder, video, '') + s for s in frames_list]
-    frames_list = [os.path.join(video_folder, s) for s in frames_list]
-    frames_list.sort()
+    frames_list = []
+    video_capture = cv2.VideoCapture(os.path.join(video_folder, os.path.basename(video_folder) + '.mp4'))
+    success, frame = video_capture.read()
+    while success:
+        success, frame = video_capture.read()
+        frames_list.append(frame)
 
     return frames_list
 
@@ -34,25 +37,22 @@ def initialize_video(video_folder):
     gt = get_groundtruth(video_folder)
     frames_list = get_frames(video_folder)
 
-    with Image.open(frames_list[0]) as img:
-        frame_size = np.flip(np.asarray(img.size))
+    assert len(frames_list) == len(gt), 'Frames and groundtruth lines numbers should be equal.'
 
-    frames_number = len(frames_list)
-    assert frames_number == len(gt), 'Frames and groundtruth lines numbers should be equal.'
-
-    return gt, frames_list, frame_size, frames_number
+    return gt, frames_list
 
 
 '''
 Test
 
-path = '../original/data/validation'
+path = 'data'
 videos = get_videos(path)
 
 for video in videos:
 
     video_folder = os.path.join(path, video)
     frames = get_frames(video_folder)
+    gt = get_frames(video_folder)
     video = initialize_video(video_folder)
     print('\n\n', video)
 '''
